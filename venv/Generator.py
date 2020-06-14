1b410c4e5dda29465c2e57baccce6fc04c694b69
DarkReef <imaganate.dark@gmail.com>
2020-06-14 23:50:46 +0500

    Generator class update, add file append function

--- venv/Generator.py
+++ venv/Generator.py
@@ -1,6 +1,16 @@
-class Generator:
+import os, time
 
-    def __init__(self, action):
-        self.action = action
+class Generator(object):
+	def __init__(self, arg):
+		super(Generator, self).__init__()
+		self.arg = arg
+
 	def FileApp(self):
-		pass
+		Character = open("Character.txt", "w")
+		ActionCards = open("ActionCards.txt", "w")
+		Bunker = open("Bunker.txt", "w")
+		Catastrophe = open("Catastrophe.txt", "w")
+
+def main():
+	Generator = Generator()
+	Generator.FileApp()
