15afd552ba3e64b21e384e49a4defc0637a1cfbf
DarkReef <imaganate.dark@gmail.com>
2020-06-17 15:53:00 +0500

    Reworked character.txt, added bunker info

--- venv/Catastrophe.py
+++ venv/Catastrophe.py
@@ -2,7 +2,9 @@ import Parser, random
 from PIL import Image, ImageDraw, ImageFont
 class Catastrophe(object):
 	CatastropheFile = "Catastrophe.txt"
+	BunkerFile = "Bunker.txt"
 	Scenario = list()
+	Bunker = {"ТОПОГРАФИЯ": "", "ОБОРУДОВАНИЕ": "", "УСТРОЙСТВО": "", "РАЗМЕР": ""}
 	ScenCount = 7
 
 	def __init__(self):
@@ -10,7 +12,14 @@ class Catastrophe(object):
 	def Generate(self):
 		mode ="["+ str(random.randint(1, self.ScenCount))+"]"
 		self.Scenario = Parser.Parse(mode, self.CatastropheFile)
+		self.GenerateBunker()
 		self.CreateImage()
+
+	def GenerateBunker(self):
+		self.Bunker["ТОПОГРАФИЯ"] =  "{}.".format(random.choice(Parser.Parse("=ТОПОГРАФИЯ=", self.BunkerFile)))
+		self.Bunker["ОБОРУДОВАНИЕ"] = "В бункере есть: {}.".format(random.choice(Parser.Parse("=ОБОРУДОВАНИЕ=", self.BunkerFile)))
+		self.Bunker["УСТРОЙСТВО"] = "Особенности бункера: {}.".format(random.choice(Parser.Parse("=УСТРОЙСТВО=", self.BunkerFile)))
+		self.Bunker["РАЗМЕР"] = "Размер бункера: {} кв. м. ".format(random.randint(150, 600))
 
 	def CreateImage(self):
 		max_w, max_h = 0, 0
@@ -23,14 +32,13 @@ class Catastrophe(object):
 			wd, hd = draw.textsize(lines)
 			if max_w < wd: max_w = wd
 			if max_h < hd: max_h = hd
-		print(max_w)
-		print(max_h)
-		img = Image.new('RGB', (max_w+100, 150), (255,255,255))
+		img = Image.new('RGB', (max_w+100, 350), (255,255,255))
 		draw = ImageDraw.Draw(img)
-
 		offset = 10
-		print(self.Scenario)
 		for line in self.Scenario:
 			draw.text((10, offset), str(line), font=font, fill=(0, 0, 0, 255))
+			offset += 30
+		for line in self.Bunker:
+			draw.text((10, offset), str(self.Bunker[line]), font=font, fill=(0, 0, 0, 255))
 			offset += 30
 		img.save('Catastrophe.png', "png")
